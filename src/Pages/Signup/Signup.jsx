d2220f6e414bf70c5ef35bc8174b5c39c077e6ed
Aakash Kumar <aakashselfmade@gmail.com>
2025-05-27 16:32:18 +0530

    Update signup page

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -23,7 +23,63 @@ const Signup = () => {
     const validateForm = () => {
         let valid = true;
         const newErrors = {...errors};
+
+        //Name validation
+        if(!formDate.name.trim()){
+            newErrors.name = 'Name is required';
+            valid = false;
+        } else{
+            newErrors.name = '';
+        }
+
+        //Email validation
+        if(!formDate.email.trim()){
+            newErrors.email = 'Email is required';
+            valid = false;
+        } else if(!validateEmail(formDate.email)){
+            newErrors.email = 'Please enter a valid email';
+            valid = false;
+        } else{
+            newErrors.email - '';
+        }
+
+        //Password validation
+        if(!formDate.password.trim()){
+            newErrors.password = 'Password is required';
+            valid = false;
+        } else if(formDate.password.length < 6){
+            newErrors.password = 'Password must be atleast 6 charecters';
+            valid = false;
+        } else{
+            newErrors.password = '';
+        }
+
+        //Confirm password validation
+        if(formDate.password !== formDate.confirmPassword){
+            newErrors.confirmPassword = 'Password do not match';
+            valid = false;
+        }else {
+            newErrors.confirmPassword = '';
+        }
+
+        setErrors(newErrors);
+        return valid;
+    };
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormDate({
+            ...formDate,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (validateForm())
     }
 }
+
 
 export default Signup;
