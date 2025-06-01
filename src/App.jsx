ab17996bd9e6720ba92b3f78f436e5b3368424d3
Aakash Kumar <aakashselfmade@gmail.com>
2025-06-01 06:37:55 +0530

    Created a Login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import Button from "./Components/Button/Button";
 import InputField from "./Components/InputField/InputField";
 import SelectField from "./Components/SelectField/SelectField";
 import Signup from "./Pages/Signup/Signup";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
 
-function App(){
-    return(
-        <>
-            <Signup/>
-        </>
+
+function App() {
+    return (
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     );
-}
+  }
 
 export default App;
--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import './Button.css'
+import './Button.css';
 
-const Button = ({ children, onClick, disabled, type = 'button'}) => {
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            disabled={disabled}
-            className="custom-button"
-        >
-            {children}
-        </button>
-    );
+const Button = ({ 
+  children, 
+  onClick, 
+  disabled, 
+  type = 'button', 
+  isLoading 
+}) => {
+  return (
+    <button 
+      type={type} 
+      onClick={onClick} 
+      disabled={disabled || isLoading}
+      className={`custom-button ${isLoading ? 'loading' : ''}`}
+      aria-busy={isLoading}
+    >
+      {isLoading && <span className="spinner" aria-hidden="true"></span>}
+      {children}
+    </button>
+  );
 };
 
 export default Button;
